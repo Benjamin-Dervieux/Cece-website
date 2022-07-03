812a39bf7d4536aee5a8155edb3e969aee562230
Dervieux <dervieux.ben@gmail.com>
2022-07-03 16:55:56 +0200

    adding code

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import style from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className={style.footerContainer}>
+      <div className={style.footer}>Made by BD</div>
+    </div>
+  );
+};
+
+export default Footer;
